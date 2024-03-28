b0dd953d5518476ab96b41ec974fb4ab9da68a68
Waqas Ahmad <waqasahmedwikki15@gmail.com>
2024-03-28 23:47:48 +0500

    constructor like shelow copy

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-var student = [];
-function addStudent() {
-  std = {
-    name: prompt("enter your name"),
-    fName: prompt("enter your fname"),
-    tName: prompt("enter your teachername"),
-  };
-  student.push(std);
-  console.log(student);
+// function addStudent() {
+//   std = {
+//     fistName: prompt("enter your fname"),
+//     lastName: prompt("enter your lname"),
+//   };
+//   studentData.push(std);
+//   console.log(studentData);
+// }
+// object without constructor
+// function studentCheck() {
+//   var check = prompt("enter number");
+//   console.log(studentData[check].fistName, studentData[check].lastName);
+// }
+var studentData = [];
+function Student(firstname, lastname, age, teacher) {
+  this.firstname = firstname;
+  this.lastname = lastname;
+  this.age = age;
+  this.teacher = teacher;
 }
+Student.prototype.getFullName = function () {
+  return `${this.firstname} ${this.lastname}`;
+};
+
+function addStudent() {
+  firstname = prompt("enter your firstname");
+  lastname = prompt("enter your lastname");
+  age = prompt("enter your age");
+  gender = prompt("enter your gender");
+}
+
+var student = new Student(firstname, lastname, age, gender);
+studentData.push(student);
+console.log("student added", student);
+console.log("all student ", studentData);
